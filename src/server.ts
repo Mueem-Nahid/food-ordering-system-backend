82f9a7e3f67b4b7376a5b2ceec696428bbc13dab
Mueem-Nahid <mueem51@gmail.com>
2025-12-27 13:26:25 +0600

    # Info - update package - remove socketio

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import { Server } from 'http';
 import app from './app';
 import config from './config';
-import { configureSocketIO } from './socketIOServer';
 
 process.on('uncaughtException', error => {
   console.log(error);
@@ -18,8 +17,6 @@ async function bootstrap() {
     console.log('Database connection successful !!!');
     server = app.listen(config.port, () => {
       console.log(`Server listening on port ${config.port} ...`);
-      // Initialize Socket.io and pass the HTTP server instance
-      configureSocketIO(server);
     });
   } catch (error) {
     console.log(`Failed to connect database.`, error);
