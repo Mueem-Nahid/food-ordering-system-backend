d7f1e02d8857e3a610ae0b3c7154a97d3494a3f7
Mueem-Nahid <mueem51@gmail.com>
2025-12-30 22:30:39 +0600

    # Fix - authorization

--- src/app/modules/addons/addons.route.ts
+++ src/app/modules/addons/addons.route.ts
@@ -10,20 +10,19 @@ const router = express.Router();
 router.post(
   '/',
   validateRequest(AddonsValidation.createAddonsZodSchema),
-  auth(ENUM_USER_ROLE.USER, ENUM_USER_ROLE.ADMIN),
+  auth(ENUM_USER_ROLE.ADMIN),
   AddonsController.createAddons,
 );
 
 router.get(
   '/',
-  auth(ENUM_USER_ROLE.USER, ENUM_USER_ROLE.ADMIN),
   AddonsController.getAllAddons,
 );
 
-router.get('/:id', auth(ENUM_USER_ROLE.USER, ENUM_USER_ROLE.ADMIN), AddonsController.getAnAddons);
+router.get('/:id', AddonsController.getAnAddons);
 
-router.patch('/:id', auth(ENUM_USER_ROLE.USER), AddonsController.updateAddons);
+router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), AddonsController.updateAddons);
 
-router.delete('/:id', auth(ENUM_USER_ROLE.USER), AddonsController.deleteAddons);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), AddonsController.deleteAddons);
 
 export const AddonsRoutes = router;
